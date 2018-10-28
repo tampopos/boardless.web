479e0cbb53452224193a02bfacc47f3e9a2badf6
wakuwaku3 <takushi_119@live.jp>
2018-10-29 03:50:34 +0900

    infinity-loadingのバグ修正

--- src/components/extensions/infinity-loading.tsx
+++ src/components/extensions/infinity-loading.tsx
@@ -23,41 +23,52 @@ interface Props {
   loadingInterval?: number;
   height?: number | string;
   next: (init: boolean) => Promise<void>;
-  anchorElm?: HTMLElement;
+  anchorElm: HTMLElement | null;
+  getExternalTrigger: (trigger: (init: boolean) => Promise<void>) => void;
 }
-interface State {}
+interface State {
+  shouldNext: boolean;
+}
 class Inner extends StyledComponentBase<typeof styles, Props, State> {
-  private nextThrottle: ThrottleAsync<boolean>;
+  private nextThrottle: ThrottleAsync<void>;
   constructor(props: any) {
     super(props);
-    this.state = {};
+    this.state = { shouldNext: false };
     const { loadingInterval } = this.props;
     this.nextThrottle = new ThrottleAsync(
       () => this.next(false),
       loadingInterval ? loadingInterval : 100,
     );
   }
+  public componentDidMount() {
+    const { getExternalTrigger } = this.props;
+    getExternalTrigger(this.next);
+  }
   private next = async (init: boolean) => {
-    const { next, anchorElm } = this.props;
+    const { next } = this.props;
+    if (init || this.isShouldNext()) {
+      await next(init);
+      while (this.isShouldNext()) {
+        await next(false);
+      }
+    }
+  };
+  private isShouldNext = () => {
+    const { anchorElm } = this.props;
     if (!anchorElm) {
       return false;
     }
     const { scrollHeight, scrollTop, offsetHeight } = anchorElm;
     const { loadCompleted } = this.props;
     const maxScroll = scrollHeight - offsetHeight;
-    if (!loadCompleted && scrollTop >= maxScroll) {
-      await next(init);
-      return true;
-    }
-    return false;
+    return !loadCompleted && scrollTop >= maxScroll;
   };
   public async componentDidUpdate?(prevProps: Readonly<Props>) {
-    const { loadCompleted, anchorElm } = this.props;
+    const { loadCompleted } = this.props;
     if (
-      anchorElm &&
-      (!prevProps ||
-        !prevProps.anchorElm ||
-        (prevProps.loadCompleted && !loadCompleted))
+      !prevProps ||
+      !prevProps.anchorElm ||
+      (prevProps.loadCompleted && !loadCompleted)
     ) {
       await this.init();
     }
@@ -69,13 +80,16 @@ class Inner extends StyledComponentBase<typeof styles, Props, State> {
     }
     anchorElm.onscroll = () => this.scrollContainer();
     await this.next(true);
-    let val = true;
-    while (val) {
-      val = await this.next(false);
-    }
   };
   public scrollContainer() {
     this.nextThrottle.execute();
+  }
+  public componentWillUnmount() {
+    const { anchorElm } = this.props;
+    if (!anchorElm) {
+      return;
+    }
+    anchorElm.onscroll = null;
   }
   public render() {
     const { children, loadCompleted, height } = this.props;
