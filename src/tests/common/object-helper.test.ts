3c589c7941114a2cde3177aa09c7f465ddbc57ac
wakuwaku3 <takushi_119@live.jp>
2018-11-03 06:28:15 +0900

    testを修正

--- src/tests/common/object-helper.test.ts
+++ src/tests/common/object-helper.test.ts
@@ -4,6 +4,6 @@ it('ObjectHelper.mapObject', () => {
   type keys = 'p1' | 'p2';
   const test: Record<keys, number> = { p1: 1, p2: 2 };
   const actual = mapObject(test, x => (x + 1).toString());
-  const expected = { p2: '2', p3: '3' };
+  const expected = { p1: '2', p2: '3' };
   expect(JSON.stringify(actual)).toEqual(JSON.stringify(expected));
 });
