79f247e136ee2d8d582f55ef196c76dc28524a00
wakuwaku3 <takushi_119@live.jp>
2018-11-03 01:20:30 +0900

    mapObjectをリファクタ

--- src/common/object-helper.ts
+++ src/common/object-helper.ts
@@ -27,7 +27,11 @@ export const mapObject = <K extends string, T, U>(
   obj: Record<K, T>,
   func: (x: T) => U,
 ): Record<K, U> => {
-  const newObj: any = Object.assign({}, obj);
-  Object.keys(obj).forEach(key => (newObj[key] = func(newObj[key])));
-  return newObj;
+  return Object.entries<T>(obj).reduce(
+    (o, [k, v]) => {
+      o[k] = func(v);
+      return o;
+    },
+    {} as Record<K, U>,
+  );
 };
